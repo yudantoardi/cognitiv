87c584c36d6bdc4d91a5cdedb20a9379fd6a2ab6
yudantoardi <yudantoardi@gmail.com>
2022-02-04 15:04:21 +0700

    updates

--- asset/js/main.js
+++ asset/js/main.js
@@ -3,4 +3,23 @@ $(document).ready(function(){
         $(".c-menu").toggleClass("show");
         $(this).toggleClass("active");
     });
+
+    $(".bt-src").click(function(){
+        $(".src-overlay").addClass("show");
+    });
+
+    var supportsTouch = 'ontouchstart' in window || navigator.msMaxTouchPoints;
+        $('.bt-src').on(supportsTouch ? 'touchend' : 'click', function (event) {
+            event.stopPropagation();
+            $('.src-overlay').addClass("show");
+            $(".master").addClass("blur");
+        });
+        $('.src').on(supportsTouch ? 'touchend' : 'click', function (ev) {
+            ev.stopPropagation();
+        });
+
+        $(document).on(supportsTouch ? 'touchend' : 'click', function (event) {
+            $('.src-overlay').removeClass("show");
+            $(".master").removeClass("blur");
+        });
 });
